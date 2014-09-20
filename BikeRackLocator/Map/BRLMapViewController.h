201f92c07e3f7200c25129df239903c478c935f4
z0kovsky <lena.pych@gmail.com>
2014-09-20 10:50:48 -0700

    add empty map view

---
+++ BikeRackLocator/Map/BRLMapViewController.h
@@ -0,0 +1,13 @@
+//
+//  BRLMapViewController.h
+//  BikeRackLocator
+//
+//  Created by Elena Pychenkova on 9/20/14.
+//  Copyright (c) 2014 com.atthack. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BRLMapViewController : UIViewController
+
+@end
---
+++ BikeRackLocator/Map/BRLMapViewController.m
@@ -0,0 +1,38 @@
+//
+//  BRLMapViewController.m
+//  BikeRackLocator
+//
+//  Created by Elena Pychenkova on 9/20/14.
+//  Copyright (c) 2014 com.atthack. All rights reserved.
+//
+
+#import "BRLMapViewController.h"
+
+@interface BRLMapViewController ()
+
+@end
+
+@implementation BRLMapViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
